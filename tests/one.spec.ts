2e4f0acda4b13332323f99675a042858f0f917a9
sebin raphael <Sebin.Raphael@xilligence.com>
2026-04-16 14:30:14 +0530

    first commit

---
+++ tests/one.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('jenkins practise', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/');
+
+  await page.locator('input[placeholder="Enter Name"]').fill('John Doe');
+  await page.waitForTimeout(2000);
+
+
+});
